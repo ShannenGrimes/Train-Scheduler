c7112f0dfc790d9d69c80cefa154fd5c79246845
Shannen Grimes <shannengrimes@yahoo.com>
2019-11-23 17:25:52 -0600

    Added Firebase connection

---
+++ assets/javascript/train.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+    apiKey: "AIzaSyC-vsHCO84y7LfP9cR9WSuz1K0ugYZNz7U",
+    authDomain: "train-scheduler-42b13.firebaseapp.com",
+    databaseURL: "https://train-scheduler-42b13.firebaseio.com",
+    projectId: "train-scheduler-42b13",
+    storageBucket: "train-scheduler-42b13.appspot.com",
+    messagingSenderId: "138381681204",
+    appId: "1:138381681204:web:7e49cb9cd47a55220b2061"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    // Variables
+      var fireDatabase = firebase.database();
+      
+
+
+})      
+
+      
+ 
